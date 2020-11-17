6a80b0253792705a29ef328dfb2fa41638300bc2
philisg <71253972+philisg@users.noreply.github.com>
2020-11-17 10:13:24 +0100

    Update Exercise.c

--- NODE2/NODE2/Exercise/Exercise.c
+++ NODE2/NODE2/Exercise/Exercise.c
@@ -1,4 +1,9 @@
-
+/**
+ * @file ADC.c
+ * @author TTK4155 2020 Group 28
+ * @date 17 nov 2020
+ * @brief File containing functions to show different exercises.
+ */
 #include "../main.h"
 
 void Exercise6(void)
