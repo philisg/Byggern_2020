2834fb8b4ba68e31a2fd489a40a1808c8b5b3fed
philisg <71253972+philisg@users.noreply.github.com>
2020-11-17 09:38:18 +0100

    Update DAC.c

--- NODE2/NODE2/Analog/DAC/DAC.c
+++ NODE2/NODE2/Analog/DAC/DAC.c
@@ -1,14 +1,22 @@
+/**
+ * @file DAC.c
+ * @author TTK4155 2020 Group 28
+ * @date 17 nov 2020
+ * @brief File containing drivers for DAC.
+ *
+ */
+
 
 #include "sam.h"
 #include "DAC.h"
 
-//Utgang på shield skal være DAC1
+//output on shield should be DAC1
 
 void DAC_init(void)
 {
-	PMC->PMC_PCER1 |= (1<<6);			// ENABLE DACC CLOCK
-	DACC->DACC_CHER = DACC_CHER_CH1; //Enable DAC1
-	DACC->DACC_MR =  DACC_MR_USER_SEL_CHANNEL1;
+	PMC->PMC_PCER1 |= (1<<6);	// ENABLE DACC CLOCK
+	DACC->DACC_CHER = DACC_CHER_CH1; //Enable DAC1 
+	DACC->DACC_MR =  DACC_MR_USER_SEL_CHANNEL1; //Use channel 1
 }
 
 void DAC_write(uint16_t data)
